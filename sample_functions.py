29e10d4b89d8aab6152917757404b7d033573b8c
Prunelle Moulaire <prunelle.moulaire@gmail.com>
2021-08-16 16:23:31 +0200

    Initial Push

---
+++ sample_functions.py
@@ -0,0 +1,6 @@
+def for_loop():
+    squared_list = []
+
+    for num in range(10):
+        squared_list.append(num**2)
+    return squared_list
---
+++ tests/test_sample_functions.py
@@ -0,0 +1,4 @@
+from sample_functions import for_loop
+
+def test_sample_functions():
+    assert type(for_loop())==list
